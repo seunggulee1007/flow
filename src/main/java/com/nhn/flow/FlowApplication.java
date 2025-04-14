4dee5e50c826d8ecd65c3d808518a41761975ab6
이승구 <leesg107@naver.com>
2025-04-14 23:25:14 +0900

    feat: redisTemplate 테스트

--- src/main/java/com/nhn/flow/FlowApplication.java
+++ src/main/java/com/nhn/flow/FlowApplication.java
@@ -1,13 +1,26 @@
 package com.nhn.flow;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+
+import java.time.Duration;
 
 @SpringBootApplication
-public class FlowApplication {
+@RequiredArgsConstructor
+public class FlowApplication implements ApplicationListener<ApplicationReadyEvent> {
+    private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(FlowApplication.class, args);
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        reactiveRedisTemplate.opsForValue().set("testKey", "testValue", Duration.ofSeconds(3)).subscribe();
     }
 
 }
