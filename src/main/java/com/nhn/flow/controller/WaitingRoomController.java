579c4704ff03061e834488b3d11e2eba7e490aa6
이승구 <leesg107@naver.com>
2025-04-16 22:59:19 +0900

    feat: 대기 화면에 현재 대기 번호 보이도록 변경

--- src/main/java/com/nhn/flow/controller/WaitingRoomController.java
+++ src/main/java/com/nhn/flow/controller/WaitingRoomController.java
@@ -1,5 +1,7 @@
 package com.nhn.flow.controller;
 
+import com.nhn.flow.service.UserQueueService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,12 +9,20 @@ import org.springframework.web.reactive.result.view.Rendering;
 import reactor.core.publisher.Mono;
 
 @Controller
+@RequiredArgsConstructor
 public class WaitingRoomController {
 
-    @GetMapping("/wating-room")
-    Mono<Rendering> waitingRoomPage(@RequestParam(name="queue", defaultValue = "default") String queue, @RequestParam(name="user_id") Long userId) {
-        return Mono.just(Rendering.view("waiting-room.html")
-                .modelAttribute("queue", queue)
-                .build());
+    private final UserQueueService userQueueService;
+
+    @GetMapping("/waiting-room")
+    Mono<Rendering> waitingRoomPage(@RequestParam(name="queue", defaultValue = "default") String queue, @RequestParam(name="user_id", required = false) Long userId) {
+        return userQueueService.registerWaitQueue(queue, userId)
+            .onErrorResume(ex -> userQueueService.getRank(queue, userId))
+            .map(rank -> Rendering.view("waiting-room.html")
+            .modelAttribute("queue", queue)
+                .modelAttribute("userId", userId)
+                .modelAttribute("number", rank)
+                .build()
+            );
     }
 }
